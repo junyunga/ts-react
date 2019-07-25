ab7fadce5042bd260c493904384192d2d3f00a1b
JunYoung Cho <cjy874545@gmail.com>
2019-07-25 22:48:26 +0900

    add submenu

--- src/core/components/header/sub_menu_list/index.tsx
+++ src/core/components/header/sub_menu_list/index.tsx
@@ -2,23 +2,47 @@ import * as React from 'react';
 import './index.scss';
 
 interface Props {
-    display: string
+    displayState: boolean,
+    subtitle: string,
+    submenu: [
+        {
+            title: string,
+            list: any[]
+        }
+
+    ]
 }
+
 class SubMenuList extends React.Component<Props> {
-
     public render() {
-
+        const { displayState, submenu, subtitle } = this.props;
         return (
-            <div className="sub-menu-list-container">
+            <div className="sub-menu-list-container" style={{ display: displayState ? '' : 'none' }}>
                 <div className="container-text-box">
-                    <h2>Solution & Service</h2>
+
+                    <h2>{subtitle}</h2>
                     <p>
                         안전한 보안세상을 만드는
                         테르텐의 보안 솔루션을 소개합니다.
                     </p>
                 </div>
                 <div className="sub-menu-list">
-                    메뉴 리스트
+                    {
+                        submenu.map((item, index) => {
+                            return (<div className="sub-menu-list__inner" key={index}>
+                                {item.title}
+                                <ul className="sub-area" key={item.title}>
+                                    {
+                                        item.list.length > 0 &&
+                                        item.list.map((subItem, subIndex) => {
+                                            return (<li key={subIndex}>{subItem}</li>)
+                                        })
+                                    }
+                                </ul>
+                            </div>);
+
+                        })
+                    }
                 </div>
             </div>
         );
--- src/layout/headerLayout/index.tsx
+++ src/layout/headerLayout/index.tsx
@@ -5,9 +5,88 @@ import SubMenuList from 'core/components/header/sub_menu_list';
 // RouteComponentProps
 
 
+interface Menu {
+    listInfo: any[]
+}
+const menu: Menu = {
+    listInfo: [{
+        show: false,
+        title: 'Sonlution&Service',
+        path: '/solution',
+        list: [{
+            title: 'Solution Map',
+            list: []
+        }, {
+            title: 'SaaS 솔루션',
+            list: []
+        }, {
+            title: '데이터 보안',
+            list: ['화면 보안', '모바일 보안', '멀티미디어 보안', 'PC 영역 보안']
+        }, {
+            title: '보안 관제',
+            list: ['웹 사이트 보안']
+        }]
+    }, {
+        show: false,
+        title: 'CustomerCenter',
+        path: '/customer',
+        list: [{
+            title: '제품문의',
+            list: []
+        }, {
+            title: 'Contact Us',
+            list: []
+        }]
+    }, {
+        show: false,
+        title: 'PR',
+        path: '/pr',
+        list: [{
+            title: '뉴스 및 공지',
+            list: []
+        }, {
+            title: '뉴스레더',
+            list: []
+        }, {
+            title: '광고',
+            list: []
+        }, {
+            title: '도입사례',
+            list: []
+        }, {
+            title: '사내소식',
+            list: []
+        }]
+    }, {
+        show: false,
+        title: 'Company',
+        path: '/company',
+        list: [{
+            title: '회사소개',
+            list: []
+        }, {
+            title: 'CEO메시지',
+            list: []
+        }, {
+            title: 'Award',
+            list: []
+        }, {
+            title: '인재채용',
+            list: []
+        }, {
+            title: '오시는 길',
+            list: []
+        }]
+    }]
+}
+
 export default class HeaderLayout extends React.Component {
+    public submenuShow = () => {
+        console.log("111")
+    }
     public render() {
         return (
+
             <header className="header">
                 <Router>
                     <div className="header__inner">
@@ -15,22 +94,16 @@ export default class HeaderLayout extends React.Component {
                             <Link to="/"><img src="" alt="회사로고" /></Link></div>
                         <nav>
                             <ul className="area">
-                                <li>
-                                    <Link to="/solution">Sonlution&Service</Link>
-                                    <SubMenuList display={'none'} />
-                                </li>
-                                <li>
-                                    <Link to="/customer">CustomerCenter</Link>
-                                    <SubMenuList display={'none'} />
-                                </li>
-                                <li>
-                                    <Link to="/pr">PR</Link>
-                                    <SubMenuList display={'none'} />
-                                </li>
-                                <li>
-                                    <Link to="/company">Company</Link>
-                                    <SubMenuList display={'none'} />
-                                </li>
+                                {
+                                    menu.listInfo.map(item => {
+                                        return (
+                                            <li className="area-list" key={item.title} onMouseOver={this.submenuShow}>
+                                                <Link to={item.path}>{item.title}</Link>
+                                                <SubMenuList displayState={item.show} submenu={item.list} subtitle={item.title} />
+                                            </li>
+                                        )
+                                    })
+                                }
                             </ul>
                         </nav>
                     </div>
