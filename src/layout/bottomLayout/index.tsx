fdc74e55ac26223332aec6ac9e4f04668249c369
JunYoung Cho <cjy874545@gmail.com>
2019-08-29 16:30:00 +0900

    footer 기본 작업

--- src/layout/bottomLayout/index.tsx
+++ src/layout/bottomLayout/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import bottomLayout from './index.scss';
 import className from 'classnames/bind';
+import logo from 'assets/moornmo.png';
 
 const c = className.bind(bottomLayout);
-// import logo from 'assets/moornmo.png';
+
 
 class BottomLayout extends React.Component {
     public render() {
@@ -16,11 +17,16 @@ class BottomLayout extends React.Component {
                     22
                 </div>
                 <div className={c('footer__comopay-info')}>
-                    <div className={c('header')}>
-                        <img src="" alt="회사로고"/>
-                    </div>
-                    <div className={c('body')}>
-                        11
+                    <div className={c('wrapper')}>
+                        <div className={c('header')}>
+                            <img src={logo} alt="회사로고" />
+                            <div><a href="/">저작권보호정책</a></div>
+                            <div><a href="/">개인정보처리방침</a></div>
+                            <div><a href="/">이메일무단수집거부</a></div>
+                        </div>
+                        <div className={c('body')}>
+                            <div className={c('address')}>부산광역시 도레미파솔라시도!!</div>
+                        </div>
                     </div>
                 </div>
             </footer>
